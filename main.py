2b1ff4f667a7204da593c7823c9efc6bd7b3f30b
gyurili <inglifestora@naver.com>
2025-05-15 13:33:53 +0900

    [Feat] retriever 기능 틀 구현 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import yaml
 
 from src.data_loader import data_load, data_process, data_chunking
 from src.vector_db import generate_vector_db, load_vector_db
-from src.retriever import retrieve_documents
+from retrieval import retrieve_documents
 # from src.generation import load_chat_model, build_qa_chain
 
 
