842bf68c3a3658bcd3be679a7f8e6967fb96ec2a
정재의 <jeaui54@gmail.com>
2025-05-19 11:51:12 +0900

    [Feat] 문서 유사도 검색 기능 추가

--- src/loader/loader_main.py
+++ src/loader/loader_main.py
@@ -1,17 +1,19 @@
 from typing import List
 from langchain.schema import Document
-from src.loader.data_loader import (data_load, data_process, data_chunking)
+from src.loader.data_loader import (retrieve_top_documents_from_metadata, data_process)
 
 
 def loader_main(config: dict) -> List[Document]:
     data_config = config.get("data", {})
+    query = config.get("retriever", {}).get("query", "사업")
+    top_k = data_config.get("top_k", 5)
+    verbose = config.get("settings", {}).get("verbose", True)
 
     # 1. 데이터 로드
     data_list_path = data_config.get("data_list_path", "data/data_list.csv")
-    limit = data_config.get("limit", 5)
 
-    df = data_load(path=data_list_path, limit=limit)
-    print("✅ 데이터 로드 완료")
+    df = retrieve_top_documents_from_metadata(query=query, csv_path=data_list_path, top_k=top_k, verbose=verbose)
+    print("✅ 문서 유사도 검색 완료")
 
     # 2. 데이터 전처리
     file_type = data_config.get("file_type", "all")
@@ -20,14 +22,15 @@ def loader_main(config: dict) -> List[Document]:
     df = data_process(df, apply_ocr=apply_ocr, file_type=file_type)
     print("✅ 데이터 전처리 완료")
 
-    # 3. 청크 생성
-    splitter_type = data_config.get("splitter", "recursive")
-    chunk_size = data_config.get("chunk_size", 300)
-    chunk_overlap = data_config.get("chunk_overlap", 50)
+    return df
+    # # 3. 청크 생성
+    # splitter_type = data_config.get("splitter", "recursive")
+    # chunk_size = data_config.get("chunk_size", 300)
+    # chunk_overlap = data_config.get("chunk_overlap", 50)
 
-    chunks = data_chunking(df=df, splitter_type=splitter_type, size=chunk_size, overlap=chunk_overlap)
-    print("✅ 청크 생성 완료")
+    # chunks = data_chunking(df=df, splitter_type=splitter_type, size=chunk_size, overlap=chunk_overlap)
+    # print("✅ 청크 생성 완료")
 
-    return chunks
+    # return chunks
 
 
