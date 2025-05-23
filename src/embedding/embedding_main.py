06be5f6a8de481dda42a07f02170502a750f2086
kyakyak <udosjdjdjdj@gmail.com>
2025-05-23 13:15:40 +0900

    [Fix] embedding_main.py 내부의 tranceable 코드 수정

--- src/embedding/embedding_main.py
+++ src/embedding/embedding_main.py
@@ -44,7 +44,7 @@ def generate_index_name(config: dict) -> str:
         return f"{data_type}_{splitter}_{model_key}_{db_type}"
 
 
-@traceable(name="embedding_main", inputs={"index_name": "<to_be_injected_at_runtime>"})
+@traceable(name="embedding_main")
 def embedding_main(config: dict, chunks: List[Document], is_save: bool = False) -> Union[FAISS, Chroma]:
     """
     설정에 따라 벡터 DB를 생성하거나 로드합니다.
