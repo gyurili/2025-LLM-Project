7b39d0aaddbdac4eb2df8846533af0f55538fa8a
kyakyak <udosjdjdjdj@gmail.com>
2025-05-26 17:31:45 +0900

    [Refactor] 리펙토링된 임베딩 코드에 맞게 테스트 코드 변경

--- embed_test.py
+++ embed_test.py
@@ -1,38 +1,37 @@
 from src.utils.shared_cache import set_cache_dirs
 set_cache_dirs()
+
 import os
 from langsmith import trace
 from dotenv import load_dotenv
+
+from src.embedding.vector_db import generate_embedding
 from src.embedding.embedding_main import embedding_main
 from src.loader.loader_main import loader_main
 from src.utils.config import load_config
 from src.utils.path import get_project_root_dir
 
 def embedding_test():
-    try: 
+    try:
         with trace(name="embedding_test") as run:
             project_root = get_project_root_dir()
-            print(f"Project root directory: {project_root}")
 
             config_path = os.path.join(project_root, "config.yaml")
-            print(f"Config file path: {config_path}")
-            
             dotenv_path = os.path.join(project_root, ".env")
             load_dotenv(dotenv_path=dotenv_path)
 
             config = load_config(project_root)
-            print("✅ Config 로드 완료")
 
             with trace(name="loader_main"):
                 chunks = loader_main(config)
-                print("✅ 데이터 로드 완료")
 
             with trace(name="embedding_main"):
-                vector_store = embedding_main(config, chunks, is_save=True)
-                print("✅ 벡터 DB 생성 완료")
+                embed_model_name = config["embedding"]["embed_model"]
+                embeddings = generate_embedding(embed_model_name)
+                vector_store = embedding_main(config, chunks, embeddings, is_save=True)
 
             run.add_outputs({
-                "embed_model": config["embedding"]["embed_model"],
+                "embed_model": embed_model_name,
                 "db_type": config["embedding"]["db_type"],
                 "vector_db_path": config["embedding"]["vector_db_path"],
                 "top_k": config["data"]["top_k"],
