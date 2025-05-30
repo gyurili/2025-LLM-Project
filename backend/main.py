3e6df9b497c50fd347032ec68510797c49856612
gyurili <inglifestora@naver.com>
2025-05-30 11:37:42 +0900

    [Fix] 공유 세션 응답 문제 해결

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
+from copy import deepcopy
 from src.generator.chat_history import load_chat_history
 from src.utils.config import load_config
 from src.utils.path import get_project_root_dir
@@ -24,6 +26,8 @@ app.add_middleware(
 class QueryRequest(BaseModel):
     query: str
     chat_history: list
+    session_id: Optional[str] = None
+    config: dict
 
 class QueryResponse(BaseModel):
     answer: str
@@ -45,17 +49,20 @@ use_quantization = config["generator"]["use_quantization"]
 model_info = get_generation_model(model_type, model_name, use_quantization)
 
 # API 호출
+from copy import deepcopy
+
 @app.post("/chat", response_model=QueryResponse)
 def chat(request: QueryRequest):
-    config["chat_history"] = request.chat_history
+    local_config = deepcopy(request.config)
+    local_config["chat_history"] = request.chat_history
 
     if request.chat_history:
-        chat_summary = load_chat_history(config, model_info)
-        config["retriever"]["query"] = f"이전 질문 요약: {chat_summary}\n질문: {request.query}"
+        chat_summary = load_chat_history(local_config, model_info)
+        local_config["retriever"]["query"] = f"이전 질문 요약: {chat_summary}\n질문: {request.query}"
     else:
-        config["retriever"]["query"] = request.query
+        local_config["retriever"]["query"] = request.query
 
-    docs, answer, elapsed = rag_pipeline(config, embeddings, request.chat_history, model_info, is_save=True, session_id=request.session_id)
+    docs, answer, elapsed = rag_pipeline(local_config, embeddings, request.chat_history, model_info, is_save=True, session_id=request.session_id)
 
     docs_result = [
         {
